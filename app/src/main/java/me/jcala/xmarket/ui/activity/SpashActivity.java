a523c5f0edb828e40b9102468a48cd8c574c3152
jcala <jcalaz@163.com>
2016-10-09 02:55:07 +0800

    添加了启动页面

---
+++ app/src/main/java/me/jcala/xmarket/ui/activity/SpashActivity.java
@@ -0,0 +1,33 @@
+package me.jcala.xmarket.ui.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import me.jcala.xmarket.R;
+
+/**
+ * Created by Administrator on 2016/10/9.
+ */
+
+public class SpashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //设置全屏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_activity);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SpashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1500);
+    }
+}
