41413d0ff1b2f74224b7ab2958998de93ed22572
jcala <jcalaz@163.com>
2016-10-13 16:52:28 +0800

    添加了侧滑栏的图标

--- app/src/main/java/me/jcala/xmarket/ui/activity/MainActivity.java
+++ app/src/main/java/me/jcala/xmarket/ui/activity/MainActivity.java
@@ -106,7 +106,7 @@ public class MainActivity  extends BaseActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main_top, menu);
         return true;
     }
 
