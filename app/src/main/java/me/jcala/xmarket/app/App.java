ff634427d1035fa4a23593cdf79aff034b216f06
jcala <jcalaz@163.com>
2016-10-14 17:39:33 +0800

    Revert "Revert "temp""
    This reverts commit 047e9e09e2c20f3363b6b3a63be700625813fc4b.

--- app/src/main/java/me/jcala/xmarket/app/App.java
+++ app/src/main/java/me/jcala/xmarket/app/App.java
@@ -3,6 +3,8 @@ package me.jcala.xmarket.app;
 import android.app.Application;
 import android.content.Context;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+
 public class App extends Application {
 
     private static Context context;
@@ -11,6 +13,7 @@ public class App extends Application {
     public void onCreate() {
         context = getApplicationContext();
         super.onCreate();
+        Fresco.initialize(this);
     }
 
     public static Context getContext() {
