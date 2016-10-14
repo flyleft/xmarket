ba978814fe6f826fff1073d20f0a4b07f3fc0d18
jcala <jcalaz@163.com>
2016-10-14 17:39:29 +0800

    Revert "Revert "Revert "找到SortFragment错误的原因，准备回滚项目"""
    This reverts commit 6e77d1dcf5ca086e7209faa53b0484523ab3a855.

--- app/src/main/java/me/jcala/xmarket/app/App.java
+++ app/src/main/java/me/jcala/xmarket/app/App.java
@@ -3,8 +3,6 @@ package me.jcala.xmarket.app;
 import android.app.Application;
 import android.content.Context;
 
-import com.facebook.drawee.backends.pipeline.Fresco;
-
 public class App extends Application {
 
     private static Context context;
@@ -13,7 +11,6 @@ public class App extends Application {
     public void onCreate() {
         context = getApplicationContext();
         super.onCreate();
-        Fresco.initialize(this);
     }
 
     public static Context getContext() {
