427b3df5cea568d6072826d9b96fd8eef3a7cfdf
jcala <jcalaz@163.com>
2016-10-15 14:50:21 +0800

    Revert "给retrofit的请求头加入了token字符串，用于验证请求"
    This reverts commit 7897784afa244be73c27006882809bc86e3aae7e.

--- app/src/main/java/me/jcala/xmarket/data/api/ReqExecutor.java
+++ app/src/main/java/me/jcala/xmarket/data/api/ReqExecutor.java
@@ -1,10 +1,7 @@
 package me.jcala.xmarket.data.api;
 
 import java.util.concurrent.TimeUnit;
-
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -15,7 +12,6 @@ import static me.jcala.xmarket.conf.NetWorkConf.DEFAULT_TIMEOUT;
 public class ReqExecutor {
     private UserReq userReq;
     private SortReq sortReq;
-    private String token="";
     private ReqExecutor(){}
     private static class ReqExecutorBuilder {
         private static ReqExecutor instance = new ReqExecutor();
@@ -25,14 +21,7 @@ public class ReqExecutor {
     }
     private Retrofit getRetrofit(){
         OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
-        httpClientBuilder.connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
-                .addInterceptor((Interceptor.Chain chain) -> {
-                Request request = chain.request();
-                Request newReq = request.newBuilder()
-                        .addHeader("Authorization",token)
-                        .build();
-                return chain.proceed(newReq);
-            });
+        httpClientBuilder.connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
         return new Retrofit.Builder()
                 .client(httpClientBuilder.build())
                 .addConverterFactory(GsonConverterFactory.create())
