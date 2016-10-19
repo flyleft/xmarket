48c5d41ca6c20fecc38de7dbd1a046e43825fe03
jcala <jcalaz@163.com>
2016-10-20 02:06:58 +0800

    添加了SchoolFragment的测试数据

--- app/src/main/java/me/jcala/xmarket/mvp/school/SchoolFragment.java
+++ app/src/main/java/me/jcala/xmarket/mvp/school/SchoolFragment.java
@@ -1,25 +1,20 @@
 package me.jcala.xmarket.mvp.school;
 
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.OrientationHelper;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
-
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import me.jcala.xmarket.R;
-import me.jcala.xmarket.data.entity.DealItem;
 import me.jcala.xmarket.mvp.a_base.BaseFragment;
 
 
 public class SchoolFragment extends BaseFragment implements SchoolView{
-    private List<DealItem> items;
+    private SchoolPresenter presenter;
+
     private Unbinder unbinder;
     @BindView(R.id.school_deal_list)
     private ListView dealList;
@@ -32,6 +27,8 @@ public class SchoolFragment extends BaseFragment implements SchoolView{
     @Override
     protected void initViews(View view, Bundle savedInstanceState) {
         unbinder= ButterKnife.bind(this,view);
+        presenter=new SchoolPresenterImpl(getActivity(),this);
+        presenter.getSchoolDealAgency();
     }
 
     @Override
--- app/src/main/java/me/jcala/xmarket/mvp/school/SchoolModelImpl.java
+++ app/src/main/java/me/jcala/xmarket/mvp/school/SchoolModelImpl.java
@@ -1,10 +1,41 @@
 package me.jcala.xmarket.mvp.school;
 
+import java.util.ArrayList;
+import java.util.List;
+import me.jcala.xmarket.conf.AppConf;
+import me.jcala.xmarket.data.entity.DealItem;
+import me.jcala.xmarket.data.entity.UserBean;
+
 public class SchoolModelImpl implements SchoolModel{
 
     @Override
     public void getSchoolDeals(int page,final onGainListener listener) {
-
+        if (AppConf.reqExcute==AppConf.reqExcuteNormal){
+            execute(listener);
+        }else {
+            executeLocal(listener);
+        }
     }
 
+    private void executeLocal(final onGainListener listener){
+        List<DealItem> itemList=new ArrayList<>();
+        UserBean bean=new UserBean();
+        bean.setAvatar_url("https://jcalaz.github.io/img/sort_book.jpg");
+        bean.setUsername("jcala");
+        DealItem item=new DealItem();
+        item.setAuthor(bean);
+        item.setPrice(999);
+        item.setTitle("JAVA编程思想");
+        item.setDescription("马勒戈壁的");
+        List<String> imags=new ArrayList<>();
+        imags.add("https://jcalaz.github.io/img/sort_life.jpg");
+        item.setImags(imags);
+        itemList.add(item);
+        listener.success(itemList);
+    }
+
+
+    private void execute(final onGainListener listener){
+
+    }
 }
