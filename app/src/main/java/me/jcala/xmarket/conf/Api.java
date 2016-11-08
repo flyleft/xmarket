42242953a65d2a5c4f8889df48fe37db658a6dbd
jcala <jcalaz@163.com>
2016-11-09 00:09:05 +0800

    根据服务器修改了Api枚举

--- app/src/main/java/me/jcala/xmarket/conf/Api.java
+++ app/src/main/java/me/jcala/xmarket/conf/Api.java
@@ -3,18 +3,15 @@ package me.jcala.xmarket.conf;
 public enum Api {
     //普通操作相关
     SUCCESS(100,"操作成功"),
-    SERVER_ERROR(101,"网络异常,请稍后再试!"),
-    FORBIDDEN(102,"没有权限"),
-    ILLEGAL_PARAMS(103,"请求参数不合法"),
-    TOKEN_ILLEGAL(105,"token不合法"),
+    PARAMS_ERROR(101,"请求参数不合法"),
+    //TOKEN_ILLEGAL(101,"token过期"),
     //用户相关
-    USER_NOT_EXIST(201,"用户不存在"),
+    USER_NOT_EXIST(201,"用户名错误"),
     USER_PASS_ERR(202,"密码错误"),
     USER_NAME_EXIST(203,"该用户名已经存在"),
     USER_PHONE_EXIST(204,"该手机号已经被注册"),
-    USER_OLD_PASS_ERR(205,"原密码错误"),
+    USER_OLD_PASS_ERR(205,"原密码错误");
     //图片相关
-    PIC_NOT_EXIST(301,"该图片资源不存在");
     private int code;
     private String msg;
 
