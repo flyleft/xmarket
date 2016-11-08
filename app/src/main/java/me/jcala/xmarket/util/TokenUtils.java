9a29ee66919edbb83b021aea5edea2fb0e618e47
jcala <jcalaz@163.com>
2016-11-08 19:42:36 +0800

    添加了token利用SharedPreferences的存储，清除，获取操作

--- app/src/main/java/me/jcala/xmarket/util/TokenUtils.java
+++ app/src/main/java/me/jcala/xmarket/util/TokenUtils.java
@@ -1,5 +1,10 @@
 package me.jcala.xmarket.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import static android.content.Context.MODE_PRIVATE;
+
 /**
  * 关于token的一些操作
  * 包括从token在sharepreference中的存取
@@ -7,13 +12,34 @@ package me.jcala.xmarket.util;
  */
 public class TokenUtils {
 
+    private final String SP="token";
+
+    private final String key="jwt";
+
     private String token="";//用于存储token的值
 
     private static class TokenHolder{
         private static TokenUtils tokenUtils=new TokenUtils();
     }
+
     public static TokenUtils instance=TokenHolder.tokenUtils;
 
+    private void saveData(final Context context,final String string){
+        SharedPreferences sp = context.getSharedPreferences(SP, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(key, string);
+        editor.apply();
+    }
 
+    private void clear(Context context){
+        SharedPreferences sp = context.getSharedPreferences(SP, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.clear();
+        editor.apply();
+    }
 
+    private String getToken(Context context){
+        SharedPreferences sp = context.getSharedPreferences(SP, MODE_PRIVATE);
+        return sp.getString(key, "");
+    }
 }
