1f1db2ed4d75642690456f479c4c9830ee5c40d4
jcala <jcalaz@163.com>
2016-11-14 22:11:12 +0800

    更改了注册页面的显示为用户名，密码，重复密码三项

--- app/src/main/java/me/jcala/xmarket/mvp/user/login/register/next/RegisterNextActivity.java
+++ app/src/main/java/me/jcala/xmarket/mvp/user/login/register/next/RegisterNextActivity.java
@@ -2,15 +2,18 @@ package me.jcala.xmarket.mvp.user.login.register.next;
 
 import android.os.Bundle;
 
+import me.jcala.xmarket.R;
 import me.jcala.xmarket.mvp.a_base.BaseActivity;
 
 public class RegisterNextActivity extends BaseActivity {
 
     @Override
     protected void initViews(Bundle savedInstanceState) {
-
+        setContentView(R.layout.register_next_activity);
+        initVariables();
     }
 
     protected void initVariables() {
+
     }
 }
