d5c07919fb3e33ca3e685a6fcc6f386d50960d5e
jcala <jcalaz@163.com>
2016-11-18 22:16:17 +0800

    增加了发布商品的Activity和布局文件，并在manifest中注册

---
+++ app/src/main/java/me/jcala/xmarket/mvp/user/trades/add/TradeAddActivity.java
@@ -0,0 +1,15 @@
+package me.jcala.xmarket.mvp.user.trades.add;
+
+import android.os.Bundle;
+
+import me.jcala.xmarket.R;
+import me.jcala.xmarket.mvp.a_base.BaseActivity;
+
+public class TradeAddActivity extends BaseActivity{
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+        setContentView(R.layout.trade_add_activity);
+    }
+
+}
