efe0a201e86930563532887834f2f2c57949cff3
jcala <jcalaz@163.com>
2016-11-20 22:35:10 +0800

    TradeAddActivity中及收入了处理成功后跳转到主页，失败后toast错误信息的方法

--- app/src/main/java/me/jcala/xmarket/mvp/user/trades/add/TradeAddActivity.java
+++ app/src/main/java/me/jcala/xmarket/mvp/user/trades/add/TradeAddActivity.java
@@ -1,11 +1,16 @@
 package me.jcala.xmarket.mvp.user.trades.add;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.github.johnpersano.supertoasts.library.Style;
+import com.github.johnpersano.supertoasts.library.SuperToast;
+import com.github.johnpersano.supertoasts.library.utils.PaletteUtils;
 import com.jph.takephoto.app.TakePhotoActivity;
 
 import me.jcala.xmarket.R;
 import me.jcala.xmarket.data.pojo.Trade;
+import me.jcala.xmarket.mvp.main.MainActivity;
 
 public class TradeAddActivity extends TakePhotoActivity implements TradeAddView{
 
@@ -20,16 +25,23 @@ public class TradeAddActivity extends TakePhotoActivity implements TradeAddView{
     }
     private void initData(){
         presenter=new TradeAddPresenterImpl(this,this);
-        presenter.tradeAdd();
+        presenter.tradeAdd(trade);
     }
 
     @Override
     public void whenAddSuccess() {
-
+        Intent intent=new Intent(TradeAddActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     @Override
     public void whenFail(String errorMsg) {
-
+        new SuperToast(TradeAddActivity.this)
+                .setText(errorMsg)
+                .setDuration(Style.DURATION_LONG)
+                .setColor(PaletteUtils.getTransparentColor(PaletteUtils.MATERIAL_RED))
+                .setAnimations(Style.ANIMATIONS_POP)
+                .show();
     }
 }
