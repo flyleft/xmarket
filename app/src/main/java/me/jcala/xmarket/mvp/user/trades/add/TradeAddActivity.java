8f3909c1a1a17c6bff6faa0780e8d6225788d033
jcala <jcalaz@163.com>
2016-11-25 12:11:30 +0800

    解决FloatActionButton在添加rxgalleryfinal添加后报错问题

--- app/src/main/java/me/jcala/xmarket/mvp/user/trades/add/TradeAddActivity.java
+++ app/src/main/java/me/jcala/xmarket/mvp/user/trades/add/TradeAddActivity.java
@@ -2,13 +2,20 @@ package me.jcala.xmarket.mvp.user.trades.add;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.github.johnpersano.supertoasts.library.Style;
 import com.github.johnpersano.supertoasts.library.SuperToast;
 import com.github.johnpersano.supertoasts.library.utils.PaletteUtils;
 
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import me.jcala.xmarket.R;
 import me.jcala.xmarket.data.pojo.Trade;
+import me.jcala.xmarket.data.pojo.TradeTag;
 import me.jcala.xmarket.mvp.a_base.BaseActivity;
 import me.jcala.xmarket.mvp.main.MainActivity;
 
@@ -17,9 +24,12 @@ public class TradeAddActivity extends BaseActivity implements TradeAddView{
     TradeAddPresenter presenter;
     Trade trade;
 
+    private List<TradeTag> tags;
+
     @Override
     protected void initViews(Bundle savedInstanceState) {
         setContentView(R.layout.trade_add_activity);
+        ButterKnife.bind(this);
         initData();
     }
 
@@ -44,4 +54,19 @@ public class TradeAddActivity extends BaseActivity implements TradeAddView{
                 .setAnimations(Style.ANIMATIONS_POP)
                 .show();
     }
+
+    @OnClick(R.id.trade_add_tag_select)
+    public void showSchoolChoice() {
+        new MaterialDialog.Builder(this)
+                .title(R.string.register_next_school_choose)
+                .items(tags)
+                .itemsCallbackSingleChoice(0,
+                        (MaterialDialog dialog, View view, int which, CharSequence text)->{
+                            //schoolName.setText(text);
+                            return true;
+                        })
+                .positiveText(R.string.register_next_choose)
+                .show();
+    }
+
 }
