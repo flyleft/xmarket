2882d7f072b27c5626fcaf6ed396527216ff98db
jcala <jcalaz@163.com>
2016-11-28 15:26:18 +0800

    添加了TestActivity仅用于测试

---
+++ app/src/main/java/me/jcala/xmarket/mvp/test/TestActivity.java
@@ -0,0 +1,14 @@
+package me.jcala.xmarket.mvp.test;
+
+import android.os.Bundle;
+
+import me.jcala.xmarket.mvp.a_base.BaseActivity;
+
+public class TestActivity extends BaseActivity{
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+
+    }
+
+}
