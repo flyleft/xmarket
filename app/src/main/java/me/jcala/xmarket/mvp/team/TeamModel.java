74849a998c1dd9c31c7246dd3e025a944e037088
jcala <jcalaz@163.com>
2016-12-01 15:16:39 +0800

    加入了SHA-256加密代码

--- app/src/main/java/me/jcala/xmarket/mvp/team/TeamModel.java
+++ app/src/main/java/me/jcala/xmarket/mvp/team/TeamModel.java
@@ -1,13 +1,14 @@
 package me.jcala.xmarket.mvp.team;
 
 import java.util.List;
-
 import me.jcala.xmarket.data.pojo.Team;
 
 public interface TeamModel {
+
     interface onGainTeamListener{
         void onSuccess(List<Team> teamList);
         void onFailure();
     }
+
     void getTeams(final onGainTeamListener listener);
 }
---
+++ app/src/main/java/me/jcala/xmarket/util/Encrypt.java
@@ -0,0 +1,69 @@
+package me.jcala.xmarket.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Encrypt
+{
+
+    /**
+     * 传入文本内容，返回 SHA-256 串
+     */
+    public String SHA256(final String strText)
+    {
+        return SHA(strText, "SHA-256");
+    }
+
+    /**
+     * 传入文本内容，返回 SHA-512 串
+     */
+    public String SHA512(final String strText)
+    {
+        return SHA(strText, "SHA-512");
+    }
+
+    /**
+     * 字符串 SHA 加密
+     */
+    private String SHA(final String strText, final String strType)
+    {
+        // 返回值
+        String strResult = null;
+
+        // 是否是有效字符串
+        if (strText != null && strText.length() > 0)
+        {
+            try
+            {
+                // SHA 加密开始
+                // 创建加密对象 并傳入加密類型
+                MessageDigest messageDigest = MessageDigest.getInstance(strType);
+                // 传入要加密的字符串
+                messageDigest.update(strText.getBytes());
+                // 得到 byte 類型结果
+                byte byteBuffer[] = messageDigest.digest();
+
+                // 將 byte 轉換爲 string
+                StringBuffer strHexString = new StringBuffer();
+                // 遍歷 byte buffer
+                for (int i = 0; i < byteBuffer.length; i++)
+                {
+                    String hex = Integer.toHexString(0xff & byteBuffer[i]);
+                    if (hex.length() == 1)
+                    {
+                        strHexString.append('0');
+                    }
+                    strHexString.append(hex);
+                }
+                // 得到返回結果
+                strResult = strHexString.toString();
+            }
+            catch (NoSuchAlgorithmException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        return strResult;
+    }
+}
