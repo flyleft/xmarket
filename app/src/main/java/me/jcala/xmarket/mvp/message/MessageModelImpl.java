ed7d8a85a06de8a319be85bb4a8c85b4ee2009d2
jcala <jcalaz@163.com>
2016-12-04 23:56:18 +0800

    MessageModel加入了确认交易的发送HTTP请求的方法

--- app/src/main/java/me/jcala/xmarket/mvp/message/MessageModelImpl.java
+++ app/src/main/java/me/jcala/xmarket/mvp/message/MessageModelImpl.java
@@ -1,7 +1,16 @@
 package me.jcala.xmarket.mvp.message;
 
+import java.util.List;
+
 import me.jcala.xmarket.AppConf;
+import me.jcala.xmarket.data.api.ReqExecutor;
+import me.jcala.xmarket.data.dto.Result;
+import me.jcala.xmarket.data.pojo.Trade;
 import me.jcala.xmarket.mock.MessageMock;
+import me.jcala.xmarket.util.CommonFactory;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MessageModelImpl implements MessageModel{
 
@@ -11,11 +20,64 @@ public class MessageModelImpl implements MessageModel{
            listener.onComplete(new MessageMock().gainMsg());
            return;
        }
+        Result result = CommonFactory.INSTANCE().server_error();
+        ReqExecutor
+                .INSTANCE()
+                .schoolReq()
+                .getSchoolTrades(schoolName,page)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Result<List<Trade>>>() {
+                    @Override
+                    public void onCompleted() {
+                        listener.onComplete(result);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        listener.onComplete(result);
+                    }
+                    @Override
+                    public void onNext(Result<List<Trade>> listResult) {
+                        result.setCode(listResult.getCode());
+                        result.setMsg(listResult.getMsg());
+                        result.setData(listResult.getData());
+                    }
+                });
+
     }
 
     @Override
     public void executeConfirmDealReq(final onMessageListener listener,
                                       final String userId,final String tradeId) {
+        if (AppConf.useMock){
+            listener.onComplete(new MessageMock().gainMsg());
+            return;
+        }
+        Result result = CommonFactory.INSTANCE().server_error();
+        ReqExecutor
+                .INSTANCE()
+                .schoolReq()
+                .getSchoolTrades(schoolName,page)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Result<List<Trade>>>() {
+                    @Override
+                    public void onCompleted() {
+                        listener.onComplete(result);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        listener.onComplete(result);
+                    }
+                    @Override
+                    public void onNext(Result<List<Trade>> listResult) {
+                        result.setCode(listResult.getCode());
+                        result.setMsg(listResult.getMsg());
+                        result.setData(listResult.getData());
+                    }
+                });
 
     }
 }
