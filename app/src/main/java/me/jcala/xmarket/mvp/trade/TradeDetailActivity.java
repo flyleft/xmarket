4f33cc2c0895dabb48e6c1ebac51970e613b12b0
jcala <jcalaz@163.com>
2016-12-07 21:22:43 +0800

    使图片轮播可以正常显示，但没有进行美化

--- app/src/main/java/me/jcala/xmarket/mvp/trade/TradeDetailActivity.java
+++ app/src/main/java/me/jcala/xmarket/mvp/trade/TradeDetailActivity.java
@@ -1,12 +1,30 @@
 package me.jcala.xmarket.mvp.trade;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.jude.rollviewpager.RollPagerView;
+import com.jude.rollviewpager.adapter.DynamicPagerAdapter;
+import com.jude.rollviewpager.adapter.StaticPagerAdapter;
+import com.jude.rollviewpager.hintview.ColorPointHintView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import me.jcala.xmarket.R;
 import me.jcala.xmarket.mvp.a_base.BaseActivity;
 
 public class TradeDetailActivity extends BaseActivity implements TradeDetailView{
+
+
+    @BindView(R.id.trade_detail_banner)
+    protected RollPagerView banner;
 
     @Override
     protected void initViews(Bundle savedInstanceState) {
@@ -17,6 +35,16 @@ public class TradeDetailActivity extends BaseActivity implements TradeDetailView
 
     @SuppressWarnings("unchecked")
     private void initData(){
+        List<String> imgList=new ArrayList<>();
+        imgList.add("https://jcalaz.github.io/img/sort_team_lunhua.jpg");
+        imgList.add("https://jcalaz.github.io/img/sort_team_students.jpg");
+        banner.setPlayDelay(2000);
+        //设置透明度
+        banner.setAnimationDurtion(500);
+        //设置适配器
+        banner.setAdapter(new TestNormalAdapter(imgList));
+
+        banner.setHintView(new ColorPointHintView(this, Color.BLACK,Color.WHITE));
     }
 
     @Override
@@ -27,5 +55,25 @@ public class TradeDetailActivity extends BaseActivity implements TradeDetailView
     @Override
     public void whenFail(String errorMsg) {
 
+    }
+    private class TestNormalAdapter extends StaticPagerAdapter {
+
+        private List<String> imgList;
+
+        public TestNormalAdapter(List<String> imgList) {
+            this.imgList = imgList;
+        }
+
+        @Override
+        public View getView(ViewGroup container, int position) {
+            SimpleDraweeView view=new SimpleDraweeView(container.getContext());
+            view.setImageURI(imgList.get(position));
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return imgList.size();
+        }
     }
 }
