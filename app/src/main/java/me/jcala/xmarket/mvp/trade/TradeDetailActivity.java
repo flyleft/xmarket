e7a78c75a6f63af4c909797f76593a286b9d4a61
jcala <jcalaz@163.com>
2016-12-08 17:48:53 +0800

    加入了产生毛玻璃效果的代码

--- app/src/main/java/me/jcala/xmarket/mvp/trade/TradeDetailActivity.java
+++ app/src/main/java/me/jcala/xmarket/mvp/trade/TradeDetailActivity.java
@@ -1,6 +1,7 @@
 package me.jcala.xmarket.mvp.trade;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
@@ -21,6 +22,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import me.jcala.xmarket.R;
 import me.jcala.xmarket.mvp.a_base.BaseActivity;
+import me.jcala.xmarket.util.FastBlurUtil;
 
 public class TradeDetailActivity extends BaseActivity implements TradeDetailView{
 
@@ -58,6 +60,15 @@ public class TradeDetailActivity extends BaseActivity implements TradeDetailView
         banner.setAdapter(new TestNormalAdapter(imgList));
         banner.setHintView(new ColorPointHintView(this, Color.BLACK,Color.WHITE));
         avatarImg.setImageURI(Uri.parse("https://jcalaz.github.io/img/sort_rent.jpg"));
+        int scaleRatio = 10;
+        int blurRadius = 8;
+//        Bitmap scaledBitmap = Bitmap.createScaledBitmap(originBitmap,
+//                originBitmap.getWidth() / scaleRatio,
+//                originBitmap.getHeight() / scaleRatio,
+//                false);
+//        Bitmap blurBitmap = FastBlurUtil.doBlur(scaledBitmap, blurRadius, true);
+//        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//        imageView.setImageBitmap(blurBitmap);
     }
 
     @Override
